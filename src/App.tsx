8453d5a1bc97e150b575060ce20e3c2b0643f2ab
Daniel Motta <danielmotta04@hotmail.com>
2024-07-02 16:35:07 -0300

    chore: react-router-dom v6 setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Outlet } from 'react-router-dom'
+
+import Navbar from './components/Navbar/Navbar'
+
+// Images
+//import Banner from './assets/images/BannerPromo.jpg'
+// import Banner2 from './assets/images/BannerPromo2.jpg'
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="w-screen h-screen">
+      <Navbar />
+      <main className="pt-24 max-w-6xl m-auto">
+        <Outlet />
+      </main>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Components/Routes
 import App from './App.tsx'
+import Home from './Pages/Home/Home.tsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
