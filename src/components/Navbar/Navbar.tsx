b56bd2167f253a097e3109a1790cf99308f20f91
Daniel Motta <danielmotta04@hotmail.com>
2024-07-03 18:06:10 -0300

    refactor: add context functions to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,15 +2,17 @@
 import Logo from '../../assets/icons/sneaker.svg'
 import Bag from '../../assets/icons/bag.svg'
 import Burguer from '../../assets/icons/burguer.svg'
+import { useShoppingCart } from '../../context/ShoppingCartContext'
 
-function Navbar() {
+const Navbar = () => {
+  const { openCart, cartQuantity } = useShoppingCart()
   return (
     <div className="w-full h-24 bg-neutral-50 fixed flex items-center place-content-evenly">
       <div>
         <img src={Logo} alt="Logo" className="w-12 h-12 md:w-16 md:h-16" />
       </div>
 
-      <div className="text-xl font-bold sm:hidden">
+      <div className="text-xl font-bold">
         <h1>Sneaker Complex</h1>
       </div>
 
@@ -24,11 +26,16 @@ function Navbar() {
       </nav>
 
       <div className="flex items-center space-x-4">
-        <div className="p-2 flex items-center hover:bg-slate-200 hover:rounded-full">
+        <div
+          onClick={openCart}
+          className="p-2 flex items-center hover:bg-slate-200 hover:rounded-full"
+        >
           <img src={Bag} alt="Bag" className="w-6 w-6 md:w-8 md:h-8" />
-          <div className="absolute w-4 h-4 bg-red-500 rounded-full flex items-center justify-center text-white text-xs translate-x-3 translate-y-2 md:translate-x-4 md:translate-y-3 border border-slate-950">
-            3
-          </div>
+          {cartQuantity && (
+            <div className="absolute w-4 h-4 bg-red-500 rounded-full flex items-center justify-center text-white text-xs translate-x-3 translate-y-2 md:translate-x-4 md:translate-y-3 border border-slate-950">
+              {cartQuantity}
+            </div>
+          )}
         </div>
 
         <div className="p-2 hover:rounded-full hover:bg-slate-200 sm:hidden">
