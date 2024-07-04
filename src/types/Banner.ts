78798f061c1e45ab6f2fd0b77b4a3c469795a6f0
Daniel Motta <danielmotta04@hotmail.com>
2024-07-04 17:39:05 -0300

    feature: add banner types

---
+++ src/types/Banner.ts
@@ -0,0 +1,13 @@
+type BannerProps = {
+  bannerId: number
+  bannerURL: string
+  bannerALT: string
+  smallBannerURL?: string
+  hasBannerText?: boolean
+  bannerText?: string
+  bannerCallText?: string
+  hasButton?: boolean
+  buttonText?: string
+}
+
+export default BannerProps
