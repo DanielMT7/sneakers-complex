445854e9abe29f86fb7742d4fd8aba1398e2f69f
Daniel Motta <danielmotta04@hotmail.com>
2024-07-04 17:38:44 -0300

    feature: add shoe types

---
+++ src/types/Shoe.ts
@@ -0,0 +1,13 @@
+type ShoeProps = {
+  shoeId: number
+  shoeBrandName: string
+  shoeURL: string
+  shoeName: string
+  shoeColorwy: string
+  shoeGenre: string
+  shoePrice: number
+  shoeDiscountedPrice?: number
+  shoeSizes: number[]
+}
+
+export default ShoeProps
