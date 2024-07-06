8ff1097d14e1b9db32c3d51eeb180315c4572435
Daniel Motta <danielmotta04@hotmail.com>
2024-07-06 15:37:31 -0300

    refactor: change html structure

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useShoppingCart } from '../../context/ShoppingCartContext'
+import { Link } from 'react-router-dom'
 
 import formatCurrency from '../../utils/formatCurrency'
 
@@ -12,9 +13,11 @@ import ShoeProps from '../../types/Shoe'
 type CartItemProps = {
   id: number
   quantity: number
+  size: number
+  handleCloseCart: () => void
 }
 
-const CartItem = ({ id, quantity }: CartItemProps) => {
+const CartItem = ({ id, quantity, size, handleCloseCart }: CartItemProps) => {
   const [shoe, setShoe] = useState<ShoeProps | null>(null)
   const { increaseCartQuantity, decreaseCartQuantity, removeFromCart } =
     useShoppingCart()
@@ -42,33 +45,52 @@ const CartItem = ({ id, quantity }: CartItemProps) => {
 
   if (!shoe) return null
 
-  const { shoeURL, shoeName, shoePrice, shoeSizes } = shoe
+  const {
+    shoeId,
+    shoeURL,
+    shoeName,
+    shoePrice,
+    shoeColorway,
+    shoeDiscountedPrice
+  } = shoe
 
   return (
     <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row items-center place-content-between space-x-4">
       <div>
-        <img className="w-48 h-48 sm:w-32 sm:h-32" src={shoeURL} />
+        <Link to={`shoe/${shoeId}`} onClick={handleCloseCart}>
+          <img className="w-48 h-48 sm:w-32 sm:h-32" src={shoeURL} />
+        </Link>
       </div>
 
       <div className="basis-2/5 md:flex-grow">
         <h1 className="font-bold">{shoeName}</h1>
-        <p className="text-sm">Tamanho: {shoeSizes[0]}</p>
-        <p className="text-sm">{formatCurrency(shoePrice)}</p>
+        <h1 className="font-medium">{shoeColorway}</h1>
+        <p className="text-sm">Tamanho: {size}</p>
+        {shoeDiscountedPrice ? (
+          <div>
+            <p className="text-sm line-through opacity-70">
+              {formatCurrency(shoePrice * quantity)}
+            </p>
+            <p>{formatCurrency(shoeDiscountedPrice * quantity)}</p>
+          </div>
+        ) : (
+          <p>{formatCurrency(shoePrice * quantity)}</p>
+        )}
       </div>
 
       <div className="flex flex-row">
         <div className="flex items-center space-x-4">
-          <button onClick={() => decreaseCartQuantity(id)}>
+          <button onClick={() => decreaseCartQuantity(id, size)}>
             <img className="w-6 h-6" src={removeIcon} alt="" />
           </button>
           <h1>{quantity}</h1>
-          <button onClick={() => increaseCartQuantity(id)}>
+          <button onClick={() => increaseCartQuantity(id, size)}>
             <img className="w-6 h-6" src={addIcon} alt="" />
           </button>
         </div>
 
         <div className="flex items-center ml-4">
-          <button onClick={() => removeFromCart(id)}>
+          <button onClick={() => removeFromCart(id, size)}>
             <img className="w-6 h-6" src={deleteIcon} alt="" />
           </button>
         </div>
