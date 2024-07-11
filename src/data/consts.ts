9218635ac3fd934e77002245b8ba109dc7af40f2
Daniel Motta <danielmotta04@hotmail.com>
2024-07-11 18:17:43 -0300

    chore: add menu and navbar const options

---
+++ src/data/consts.ts
@@ -0,0 +1,258 @@
+// Navbar Options
+export const NavbarBrandOptions = [
+  {
+    option: 'Nike',
+    url: 'offer/brand/nike',
+    subOptions: [
+      {
+        subLabel: 'Gênero',
+        options: [
+          {
+            subOption: 'Masculino',
+            subUrl: 'offer/gender/masculino/brand/nike'
+          },
+          {
+            subOption: 'Feminino',
+            subUrl: 'offer/gender/feminino/brand/nike'
+          },
+          {
+            subOption: 'Unissex',
+            subUrl: 'offer/gender/unissex/brand/nike'
+          }
+        ]
+      },
+      {
+        subLabel: 'Categoria',
+        options: [
+          {
+            subOption: 'Corrida',
+            subUrl: 'offer/brand/nike/category/corrida'
+          },
+          {
+            subOption: 'Basquete',
+            subUrl: 'offer/brand/nike/category/basquete'
+          },
+          {
+            subOption: 'Casual',
+            subUrl: 'offer/brand/nike/category/casual'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    option: 'Adidas',
+    url: 'offer/brand/adidas',
+    subOptions: [
+      {
+        subLabel: 'Gênero',
+        options: [
+          {
+            subOption: 'Masculino',
+            subUrl: 'offer/gender/masculino/brand/adidas'
+          },
+          {
+            subOption: 'Feminino',
+            subUrl: 'offer/gender/feminino/brand/adidas'
+          },
+          {
+            subOption: 'Unissex',
+            subUrl: 'offer/gender/unissex/brand/adidas'
+          }
+        ]
+      },
+      {
+        subLabel: 'Categoria',
+        options: [
+          {
+            subOption: 'Corrida',
+            subUrl: 'offer/brand/adidas/category/corrida'
+          },
+          {
+            subOption: 'Basquete',
+            subUrl: 'offer/brand/adidas/category/basquete'
+          },
+          {
+            subOption: 'Casual',
+            subUrl: 'offer/brand/adidas/category/casual'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    option: 'Asics',
+    url: 'offer/brand/asics',
+    subOptions: [
+      {
+        subLabel: 'Gênero',
+        options: [
+          {
+            subOption: 'Masculino',
+            subUrl: 'offer/gender/masculino/brand/asics'
+          },
+          {
+            subOption: 'Feminino',
+            subUrl: 'offer/gender/feminino/brand/asics'
+          },
+          {
+            subOption: 'Unissex',
+            subUrl: 'offer/gender/unissex/brand/asics'
+          }
+        ]
+      },
+      {
+        subLabel: 'Categoria',
+        options: [
+          {
+            subOption: 'Corrida',
+            subUrl: 'offer/brand/asics/category/corrida'
+          },
+          {
+            subOption: 'Basquete',
+            subUrl: 'offer/brand/asics/category/basquete'
+          },
+          {
+            subOption: 'Casual',
+            subUrl: 'offer/brand/asics/category/casual'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    option: 'New Balance',
+    url: 'offer/brand/newbalance',
+    subOptions: [
+      {
+        subLabel: 'Gênero',
+        options: [
+          {
+            subOption: 'Masculino',
+            subUrl: 'offer/gender/masculino/brand/newbalance'
+          },
+          {
+            subOption: 'Feminino',
+            subUrl: 'offer/gender/feminino/brand/newbalance'
+          },
+          {
+            subOption: 'Unissex',
+            subUrl: 'offer/gender/unissex/brand/newbalance'
+          }
+        ]
+      },
+      {
+        subLabel: 'Categoria',
+        options: [
+          {
+            subOption: 'Corrida',
+            subUrl: 'offer/brand/newbalance/category/corrida'
+          },
+          {
+            subOption: 'Basquete',
+            subUrl: 'offer/brand/newbalance/category/basquete'
+          },
+          {
+            subOption: 'Casual',
+            subUrl: 'offer/brand/newbalance/category/casual'
+          }
+        ]
+      }
+    ]
+  }
+]
+
+// Menu Options
+export const menuOptions = [
+  {
+    option: 'Ofertas',
+    url: '/offer'
+  },
+  {
+    option: 'Promoção',
+    url: '/offer'
+  },
+  {
+    option: 'Marcas',
+    subOptions: [
+      {
+        option: 'Nike',
+        subOptions: [
+          {
+            option: 'Gênero',
+            subOptions: [
+              {
+                option: 'Masculino',
+                url: 'offer/gender/masculino/brand/nike'
+              },
+              {
+                option: 'Feminino',
+                url: 'offer/gender/feminino/brand/nike'
+              },
+              {
+                option: 'Unissex',
+                url: 'offer/gender/unissex/brand/nike'
+              }
+            ]
+          },
+          {
+            option: 'Categoria',
+            subOptions: [
+              {
+                option: 'Corrida',
+                url: 'offer/brand/nike/category/corrida'
+              },
+              {
+                option: 'Basquete',
+                url: 'offer/brand/nike/category/basquete'
+              },
+              {
+                option: 'Casual',
+                url: 'offer/brand/nike/category/casual'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        option: 'Adidas',
+        subOptions: [
+          {
+            option: 'Gênero',
+            subOptions: [
+              {
+                option: 'Masculino',
+                url: 'offer/gender/masculino/brand/adidas'
+              },
+              {
+                option: 'Feminino',
+                url: 'offer/gender/feminino/brand/adidas'
+              },
+              {
+                option: 'Unissex',
+                url: 'offer/gender/unissex/brand/adidas'
+              }
+            ]
+          },
+          {
+            option: 'Categoria',
+            subOptions: [
+              {
+                option: 'Corrida',
+                url: 'offer/brand/adidas/category/corrida'
+              },
+              {
+                option: 'Basquete',
+                url: 'offer/brand/adidas/category/basquete'
+              },
+              {
+                option: 'Casual',
+                url: 'offer/brand/adidas/category/casual'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+]
